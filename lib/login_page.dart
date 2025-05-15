fd7d8b1846d29bb578d18db1fec3db14fcc5e056
unknown <kmbs2004@gmail.com>
2025-05-15 21:57:40 +0800

    Organizing the syles and colors for the chat page

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,6 +1,7 @@
 import 'dart:js';
 
 import 'package:chat_app/chat_page.dart';
+import 'package:chat_app/utils/spaces.dart';
 import 'package:chat_app/utils/textfield_styles.dart';
 import 'package:chat_app/widgets/login_text_field.dart';
 import 'package:flutter/material.dart';
@@ -82,9 +83,7 @@ class LoginPage extends StatelessWidget {
                     },
                     controller: userNameController,
                   ),
-                  SizedBox(
-                    height: 24,
-                  ),
+                  verticalSpacing(24),
                   LoginTextField(
                     hasAsterisks: true,
                     hintText: 'Enter your password',
@@ -93,9 +92,7 @@ class LoginPage extends StatelessWidget {
                 ],
               ),
             ),
-            SizedBox(
-              height: 24,
-            ),
+            verticalSpacing(24),
             ElevatedButton(
                 onPressed: () {
                   loginUser(context);
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:chat_app/chat_page.dart';
 import 'package:chat_app/counter_stateful_demo.dart';
 import 'package:chat_app/login_page.dart';
+import 'package:chat_app/utils/brand_color.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,7 +15,7 @@ class ChatApp extends StatelessWidget {
     return MaterialApp(
       title: "Chat App",
       theme: ThemeData(
-          primarySwatch: Colors.deepPurple,
+          primarySwatch: BrandColor.primaryColor,
           appBarTheme: AppBarTheme(
               backgroundColor: Colors.blue, foregroundColor: Colors.black)),
       home: LoginPage(),
