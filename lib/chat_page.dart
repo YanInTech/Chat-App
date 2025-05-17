6b5975f67fce776d5723ddcfada5c1b140c1fb53
unknown <kmbs2004@gmail.com>
2025-05-17 23:55:31 +0800

    Reading data from a JSON file

--- lib/chat_page.dart
+++ lib/chat_page.dart
@@ -1,34 +1,50 @@
+import 'dart:convert';
+
 import 'package:chat_app/models/chat_message_entity.dart';
 import 'package:chat_app/widgets/chat_bubble.dart';
 import 'package:chat_app/widgets/chat_input.dart';
 import 'package:flutter/material.dart';
 import 'package:chat_app/widgets/chat_bubble.dart';
+import 'package:flutter/services.dart';
 
-class ChatPage extends StatelessWidget {
+class ChatPage extends StatefulWidget {
   ChatPage({super.key});
 
-  List<ChatMessageEntity> _messages = [
-    ChatMessageEntity(
-      author: Author(userName: 'hakdog'),
-      createdAt: 2132211242,
-      id: '1',
-      text: 'First text'),
-    ChatMessageEntity(
-      author: Author(userName: 'hakdog'),
-      createdAt: 2132211442,
-      id: '1',
-      text: 'Second text',
-      imageUrl: 'https://th.bing.com/th/id/R.73e9d869b7824956881985300bad12ea?rik=HECJEhmoQWhz9A&riu=http%3a%2f%2fwww.clipartbest.com%2fcliparts%2fniX%2fo77%2fniXo7745T.png&ehk=STYDZV4xsoPe%2f2NBzJlYayG8lIemvAO3Hac%2fK9N13oY%3d&risl=&pid=ImgRaw&r=0'),
-    ChatMessageEntity(
-      author: Author(userName: 'oki'),
-      createdAt: 2132211242,
-      id: '1',
-      text: 'Third text'),
-    
-  ];
+  @override
+  State<ChatPage> createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  //initiate state of messages
+  List<ChatMessageEntity> _messages = [];
+
+  _loadInitialMessages() async {
+    final response = await rootBundle.loadString('assets/mock_messages.json');
+
+    final List<dynamic> decodeList = jsonDecode(response) as List;
+
+    final List<ChatMessageEntity> _chatMessages = decodeList.map((listItem) {
+      return ChatMessageEntity.fromJson(listItem);
+    }).toList();
+
+    print(_chatMessages.length);
+
+    //final state of messages
+    setState(() {
+      _messages = _chatMessages;
+    });
+  }
+
+  @override
+  void initState() {
+    _loadInitialMessages();
+    // TODO: implement initState
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
+    _loadInitialMessages();
     final username = ModalRoute.of(context)!.settings.arguments as String;
 
     return Scaffold(
