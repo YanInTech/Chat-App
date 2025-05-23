49dc3ce14913081db906c8c979e91584309edb8f
unknown <kmbs2004@gmail.com>
2025-05-23 21:59:44 +0800

    Caching data

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,13 +1,25 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
 class AuthService {
-  void loginUser() {
-
+  final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+  
+  Future<void> loginUser(String userName) async {
+    try {
+      SharedPreferences sharedPrefs = await _prefs;
+      sharedPrefs.setString('userName', userName);  
+    } catch (e) {
+      print(e);
+    }
+    
   }
 
-  void logoutUser() {
-
+  void logoutUser() async {
+    SharedPreferences sharedPrefs = await _prefs;
+    sharedPrefs.clear();
   }
 
-  String getUserName() {
-    return "hakdog";
+  Future<String?> getUserName() async {
+    SharedPreferences sharedPrefs = await _prefs;
+    return sharedPrefs.getString('userName') ?? 'DefaultValue';
   }
 }
