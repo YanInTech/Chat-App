8e04aa584b38945f70b1886f55d7ed2608201cf0
unknown <kmbs2004@gmail.com>
2025-05-23 22:37:35 +0800

    Static initializers

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,9 @@ import 'package:chat_app/utils/brand_color.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await AuthService.init();
   runApp(Provider(
     create: (BuildContext context) => AuthService(),
     child: ChatApp(),
