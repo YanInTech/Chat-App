629897823882762e15929d211907fc73820e36fe
unknown <kmbs2004@gmail.com>
2025-05-23 23:25:59 +0800

    Creating a dynamic home page

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,18 @@ class ChatApp extends StatelessWidget {
           primarySwatch: Colors.deepPurple,
           appBarTheme: AppBarTheme(
               backgroundColor: Colors.blue, foregroundColor: Colors.black)),
-      home: LoginPage(),
+      home: FutureBuilder<bool>(
+        future: context.read<AuthService>().isLoggedin(),
+        builder: (context, AsyncSnapshot<bool> snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            if (snapshot.hasData && snapshot.data!) {
+              return ChatPage();
+            } else
+              return LoginPage();
+          }
+          return CircularProgressIndicator();
+        }
+      ),
       routes: {'/chat': (context) => ChatPage()},
     );
   }
